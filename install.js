4c511c5a3dd21e43233fc0849b0d8c5ed6e4c128
Ahmed Sharyph <39231530+AhmedSharyph@users.noreply.github.com>
2025-07-25 12:11:05 +0500

    Update install.js

--- install.js
+++ install.js
@@ -6,7 +6,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
   const box = document.createElement('div');
   box.innerHTML = `
     <div style="position:fixed;bottom:20px;left:20px;right:20px;background:#fff;padding:14px;border-radius:12px;box-shadow:0 0 12px rgba(0,0,0,0.3);z-index:9999;text-align:center;">
-      <p>📲 Add FunaTrace to your Home Screen</p>
+      <p>📲 Add Utheemu Mapper to your Home Screen</p>
       <button style="padding:10px 18px;background:#00bcd4;color:#fff;border:none;border-radius:6px;font-size:16px;" id="installBtn">Add</button>
     </div>`;
   document.body.appendChild(box);
